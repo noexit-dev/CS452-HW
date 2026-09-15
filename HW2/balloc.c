49cedf171309b1abca930a783a58da14f82e10c2
noexit-dev <Ryhilde@outlook.com>
2026-09-15 16:34:12 -0600

    balloc WIP

--- HW2/balloc.c
+++ HW2/balloc.c
@@ -4,67 +4,53 @@
 #include "freelist.h"
 #include <math.h>
 
+BRep brep;
 
 typedef struct {
     void * baseaddr;
-    int lower_order;
-    int upper_order;
-    unsigned int poolsize;   
-    BlockList blocklist;
-    FreeList freelist;
+    void * metadataaddr;
+    void * freelistaddr;
 } *BRep; //balloc representation
 
 typedef struct {
-    unsigned int blocklist;
-} *BlockList;
-
-extern BlockList blocklistcreate(int l, unsigned int poolsize){
-    
-
-}
-//organize metadata as the following: l / u / poolsize / baseaddr (8 bytes) / blocklist (struct unsigned int arr of length l) / freelist (struct of ptr (8 bytes) and bitmap (char * which is going to be 2^l+1 / 8 to find amount of bytes))
-//u-l creates size of freelist arr
+    int lower_order;
+    int upper_order;
+    unsigned int poolsize;  
+} *Metadata;
 
 size_t metasize(int u, int l, unsigned int poolsize){
     size_t baseaddr_l_u_poolsize = sizeof(void *) + (sizeof(int) * 2) + sizeof(unsigned int);
-    
-    size_t head_ptrs_size = (u-l)* sizeof(void *);
-    size_t bitmap_size = 0;
-    for(int i = u; i < l; i--){
-        size_t pairs = poolsize / e2size(i+1);
-        bitmap_size += ceil(((double)pairs)/8);
-    }
-    size_t freelist_size = head_ptrs_size + bitmap_size;
-
     size_t blocklist_size = ((poolsize / e2size(l)) * 4);
-
-    size_t metasize = baseaddr_l_u_poolsize + freelist_size + blocklist_size;
-
+    size_t metasize = baseaddr_l_u_poolsize + blocklist_size;
     return metasize;
 }
 
 extern Balloc bcreate(unsigned int size, int l, int u){
-    //set lower order value somewhere (maybe even upper value)
-    size_t meta = metasize(u,l,size);
-    BlockList blocklist;
-
-    BRep brep = mmalloc(meta+size); //alloc space for the struct
-    brep->baseaddr = brep+meta; //find with offset of metasize maybe 
-    brep->lower_order = l;
-    brep->upper_order = u;
-    brep->poolsize = lower_power_of_2(size);
-    brep->freelist = freelistcreate(size, l, u);
-    brep->blocklist = blocklist;
-    //in order to get size of meta + pool i need size of meta
-
-    brep->blocklist->;
-
-
+    //create poolsize rounding to lowest power of 2
+    unsigned int poolsize = lower_power_of_2(size); //TODO round this to higher power of 2
     
+    //allocating data
+    Metadata * metadata = mmalloc(metasize(u,l,size));
+    FreeList * freelist = freelistcreate(size, l, u);
+    
+    Balloc * pool = mmalloc(poolsize);
+    
+    //storing metadata
+    brep->metadataaddr = metadata;
+    brep->metadataaddr->lower_order = l;
+    brep->metadataaddr->upper_order = u;
+    brep->metadataaddr->poolsize = poolsize;
+    brep->baseaddr = pool;
+
+    brep->freelistaddr = freelist;
+    brep->freelistaddr[u-l]->head = pool;
+    brep->freelistaddr[u-l]->freebm = bbmcreate(poolsize,u-l);
+
+    return (Balloc) brep;
 }
 
 extern void bdelete(Balloc pool){
-    bbmdelete(pool);
+    
 }
 
 //allocates mem of size bytes from pool (bcreate)
@@ -73,6 +59,7 @@ extern void *balloc(Balloc pool, unsigned int size){
         printf("Error: size request is larger than u arg");
         return NULL; //TODO: Check if this is right
     }
+
     //check order of size
     int order = size2e(size);
 
--- HW2/freelist.c
+++ HW2/freelist.c
@@ -4,17 +4,29 @@
 typedef struct {
     void * head;
     unsigned char * freebm; //byte representation
-} *FreeList;
+} FreeListElement;
 
-//uses linked list (deq?) stores a linked list of free page blocks
-// and a bit map representing the free and used blocks
+size_t freelistsize(int u, int l, unsigned int poolsize){
+    size_t head_ptrs_size = (u-l)* sizeof(void *);
+    size_t bitmap_size = 0;
+    for(int i = u; i < l; i--){
+        size_t pairs = poolsize / e2size(i+1);
+        bitmap_size += ceil(((double)pairs)/8);
+    }
+    size_t freelist_size = head_ptrs_size + bitmap_size;
+}
+
 extern FreeList freelistcreate(size_t size, int l, int u){
-    FreeList freelist;
+    FreeList * freelist = mmalloc(sizeof(FreeListElement) * (u-l));
     return freelist;
 }
 
 extern void freelistdelete(FreeList f, int l, int u){
+    
+}
 
+extern void *freelistalloc(FreeList f, void *base, int e, int l){
+    
 }
 
 extern void  freelistfree(FreeList f, void *base, void *mem, int e, int l){
@@ -29,7 +41,3 @@ extern void freelistprint(FreeList f, int l, int u){
     
 }
 
-// extern void *freelistalloc(FreeList f, void *base, int e, int l){
-    
-
-// }
